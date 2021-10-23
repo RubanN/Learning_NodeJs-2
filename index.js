a334c71981e0ace62dda9ddbdd4640228c822b95
Ruban <80041905+RubanN@users.noreply.github.com>
2021-10-23 10:19:35 +0530

    NetNinja Node js is done

---
+++ index.js
@@ -0,0 +1,6 @@
+// module and require
+// var stuff = require('./stuff.js');
+
+// console.log(stuff.counter(['foo', 'bar', 'baz']));
+// console.log(stuff.adder(9, 7))
+// console.log(stuff.adder(stuff.pi, 5))
