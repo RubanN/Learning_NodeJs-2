7da28f6ed6af3a7ba48fa24436049272ca1103a3
Ruban <80041905+RubanN@users.noreply.github.com>
2021-10-23 13:21:55 +0530

    code runner is installed

--- index.js
+++ index.js
@@ -4,3 +4,27 @@
 // console.log(stuff.counter(['foo', 'bar', 'baz']));
 // console.log(stuff.adder(9, 7))
 // console.log(stuff.adder(stuff.pi, 5))
+// var name = 'ruban'
+// setTimeout((x) => {
+//     x = "ruban"
+//     console.log(x);
+// }, 2000)
+
+//Http
+// var http = require('http');
+// http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.write("Hello ruban")
+//     res.end()
+// }).listen(3000)
+
+//events
+// var fs = require('fs');
+// fs.readFile('text.txt', 'hello world', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Async data is" + data.toString());
+//     }
+// })
+console.log("hhh");
